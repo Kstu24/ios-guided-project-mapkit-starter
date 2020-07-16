73589be9bd2d5dbdf388ed7f7863ed7fd0d6c03c
Kevin Stewart <kistewart419@gmail.com>
2020-07-15 22:26:13 -0400

    Added user location and updated quakes to be recorded to API

--- Quakes/EarthquakesViewController.swift
+++ Quakes/EarthquakesViewController.swift
@@ -11,10 +11,48 @@ import MapKit
 
 class EarthquakesViewController: UIViewController {
     
+    private let quakeFetcher = QuakeFetcher()
+    
     @IBOutlet var mapView: MKMapView!
+    private var userTrackingButton: MKUserTrackingButton!
+    
+    private let locationManager = CLLocationManager()
 	
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        locationManager.requestWhenInUseAuthorization()
+        
+        userTrackingButton = MKUserTrackingButton(mapView: mapView)
+        userTrackingButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(userTrackingButton)
+        
+        NSLayoutConstraint.activate([
+            userTrackingButton.leadingAnchor.constraint(equalTo: mapView.leadingAnchor,
+                                                        constant: 20),
+            mapView.bottomAnchor.constraint(equalTo: userTrackingButton.bottomAnchor,
+                                            constant: 20)
+        ])
+        fetchQuakes()
     }
+    
+    
+    func fetchQuakes() {
+        let visibleRegion = mapView.visibleMapRect
+        
+        quakeFetcher.fetchQuakes(in: visibleRegion) { (quakes, error) in
+            if let error = error {
+                NSLog("%@", "Error fetching quakes: \(error)")
+            }
+            print(quakes)
+        }
+    }
+}
+
+extension EarthquakesViewController: MKMapViewDelegate {
+    
+    func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+        fetchQuakes()
+    }
+    
 }
